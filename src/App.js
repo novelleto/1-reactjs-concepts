beb1ec785e5f17c0b1b990a78923a652503b451e
Guilherme Novelleto <guilherme.novelleto@gmail.com>
2020-05-22 16:44:49 -0400

    first update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, updtRepositories] = useState([]);
+  
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      updtRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: '',
+      url: '',
+      tech: []
+    })
+
+    updtRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    updtRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))  
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+         <li key={repository.id}>
+           {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+      </li>
+      ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
